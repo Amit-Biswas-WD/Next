92e2cfc934c3f8089a4b4437761d9fc301940f70
unknown <amitbiswas9282@gmail.com>
2024-07-15 13:35:32 +0600

    This is Comment

--- app/lib/getAllPost.js
+++ app/lib/getAllPost.js
@@ -2,5 +2,10 @@ export default async function getAllPost() {
   const result = await fetch(
     "https://jsonplaceholder.typicode.com/posts?_limit=10"
   );
+
+  if (!result.ok) {
+    throw new Error("There was in error fetching error");
+  }
+
   return result.json();
 }
--- app/posts/[id]/page.js
+++ app/posts/[id]/page.js
@@ -1,15 +1,29 @@
 import getPost from "@/app/lib/getPost";
+import getPostComments from "@/app/lib/getPostComment";
 import React from "react";
 
-async function PostPage({ params }) {
+export async function generateMetadata({ params }) {
   const { id } = params;
   const post = await getPost(id);
+
+  return {
+    title: post.title,
+    description: post.body,
+  };
+}
+
+export default async function PostPage({ params }) {
+  const { id } = params;
+  const postPromise = getPost(id);
+  const commentPromise = getPostComments(id);
+
+  const [post, comment] = await Promise.all([postPromise, commentPromise]);
+
   return (
     <div>
       <h2>{post.title}</h2>
       <p>{post.body}</p>
+      <hr />
     </div>
   );
 }
-
-export default PostPage;
